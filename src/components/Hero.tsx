1f6181c2676f8ecf48b9f41b7d842cbd0ba009f3
Rappykyun <rakuichijo155@gmail.com>
2025-01-24 20:16:53 +0800

    fixed munor layout hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,34 @@
 import HeroSvg from "../assets/icons/HeroSvg";
-
+import { github, linkedin, facebook, mail, Github, Link } from "lucide-react";
 export function Hero() {
+    const socials = [
+      {
+        name: "Github",
+        icon: github, // Now using the imported Github component
+        url: "https://github.com/Rappykyun",
+      },
+      {
+        name: "LinkedIn",
+        icon: linkedin, // Now using the imported Github component
+        url: "https://github.com/yourusername",
+      },
+    ];
   return (
     <main className="max-w-7xl mx-auto md:px-16 px-6 lg:mt-32 mt-20">
-      <section className="flex flex-col lg:flex-row items-center justify-between gap-16">
-        <div className="lg:w-4/5 lg:pr-8">
+      <section className="flex flex-col lg:flex-row items-start justify-between gap-16">
+        <div className="lg:w-3/5">
           <div className="mb-8 relative">
             <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-green-500/20 relative z-10">
               <div className="w-full h-full bg-gradient-to-br from-green-400 to-blue-500 animate-gradient"></div>
             </div>
           </div>
 
-          <h1
-            className="font-incognito font-semibold tracking-tight text-3xl 
-            sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight lg:max-w-lg"
-          >
+          <h1 className="font-incognito font-semibold tracking-tight text-3xl sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight">
             Computer Science Student and Aspiring Software Engineer based in
             Sultan Kudarat, Philippines.
           </h1>
 
-          <p className="text-lg sm:text-xl text-gray-600 dark:text-gray-400 mb-8 lg:max-w-lg">
+          <p className="text-lg sm:text-xl text-gray-600 dark:text-gray-400 mb-8">
             I am Ralph Vincent C. Rodriguez a Computer Science student at Sultan
             Kudarat State University and I am passionate about software
             development. I am currently learning web development and I am
@@ -28,8 +37,8 @@ export function Hero() {
           </p>
         </div>
 
-        <div className="lg:w-3/5">
-          <div className="animate-float">
+        <div className="lg:w-2/5 flex justify-center items-center">
+          <div className="animate-float mt-16 lg:mt-24">
             <HeroSvg />
           </div>
         </div>
